451b41139cafab5cfbcee057e0e470479bc4a18c
Raffaele Di Martino <raf@raffaeles-mbp.home>
2022-12-21 20:27:13 +0000

    refactoring previous code

--- index.js
+++ index.js
@@ -1,154 +1,148 @@
 var finances = [
-['Jan-2010', 867884],
-['Feb-2010', 984655],
-['Mar-2010', 322013],
-['Apr-2010', -69417],
-['May-2010', 310503],
-['Jun-2010', 522857],
-['Jul-2010', 1033096],
-['Aug-2010', 604885],
-['Sep-2010', -216386],
-['Oct-2010', 477532],
-['Nov-2010', 893810],
-['Dec-2010', -80353],
-['Jan-2011', 779806],
-['Feb-2011', -335203],
-['Mar-2011', 697845],
-['Apr-2011', 793163],
-['May-2011', 485070],
-['Jun-2011', 584122],
-['Jul-2011', 62729],
-['Aug-2011', 668179],
-['Sep-2011', 899906],
-['Oct-2011', 834719],
-['Nov-2011', 132003],
-['Dec-2011', 309978],
-['Jan-2012', -755566],
-['Feb-2012', 1170593],
-['Mar-2012', 252788],
-['Apr-2012', 1151518],
-['May-2012', 817256],
-['Jun-2012', 570757],
-['Jul-2012', 506702],
-['Aug-2012', -1022534],
-['Sep-2012', 475062],
-['Oct-2012', 779976],
-['Nov-2012', 144175],
-['Dec-2012', 542494],
-['Jan-2013', 359333],
-['Feb-2013', 321469],
-['Mar-2013', 67780],
-['Apr-2013', 471435],
-['May-2013', 565603],
-['Jun-2013', 872480],
-['Jul-2013', 789480],
-['Aug-2013', 999942],
-['Sep-2013', -1196225],
-['Oct-2013', 268997],
-['Nov-2013', -687986],
-['Dec-2013', 1150461],
-['Jan-2014', 682458],
-['Feb-2014', 617856],
-['Mar-2014', 824098],
-['Apr-2014', 581943],
-['May-2014', 132864],
-['Jun-2014', 448062],
-['Jul-2014', 689161],
-['Aug-2014', 800701],
-['Sep-2014', 1166643],
-['Oct-2014', 947333],
-['Nov-2014', 578668],
-['Dec-2014', 988505],
-['Jan-2015', 1139715],
-['Feb-2015', 1029471],
-['Mar-2015', 687533],
-['Apr-2015', -524626],
-['May-2015', 158620],
-['Jun-2015', 87795],
-['Jul-2015', 423389],
-['Aug-2015', 840723],
-['Sep-2015', 568529],
-['Oct-2015', 332067],
-['Nov-2015', 989499],
-['Dec-2015', 778237],
-['Jan-2016', 650000],
-['Feb-2016', -1100387],
-['Mar-2016', -174946],
-['Apr-2016', 757143],
-['May-2016', 445709],
-['Jun-2016', 712961],
-['Jul-2016', -1163797],
-['Aug-2016', 569899],
-['Sep-2016', 768450],
-['Oct-2016', 102685],
-['Nov-2016', 795914],
-['Dec-2016', 60988],
-['Jan-2017', 138230],
-['Feb-2017', 671099]
+    ['Jan-2010', 867884],
+    ['Feb-2010', 984655],
+    ['Mar-2010', 322013],
+    ['Apr-2010', -69417],
+    ['May-2010', 310503],
+    ['Jun-2010', 522857],
+    ['Jul-2010', 1033096],
+    ['Aug-2010', 604885],
+    ['Sep-2010', -216386],
+    ['Oct-2010', 477532],
+    ['Nov-2010', 893810],
+    ['Dec-2010', -80353],
+    ['Jan-2011', 779806],
+    ['Feb-2011', -335203],
+    ['Mar-2011', 697845],
+    ['Apr-2011', 793163],
+    ['May-2011', 485070],
+    ['Jun-2011', 584122],
+    ['Jul-2011', 62729],
+    ['Aug-2011', 668179],
+    ['Sep-2011', 899906],
+    ['Oct-2011', 834719],
+    ['Nov-2011', 132003],
+    ['Dec-2011', 309978],
+    ['Jan-2012', -755566],
+    ['Feb-2012', 1170593],
+    ['Mar-2012', 252788],
+    ['Apr-2012', 1151518],
+    ['May-2012', 817256],
+    ['Jun-2012', 570757],
+    ['Jul-2012', 506702],
+    ['Aug-2012', -1022534],
+    ['Sep-2012', 475062],
+    ['Oct-2012', 779976],
+    ['Nov-2012', 144175],
+    ['Dec-2012', 542494],
+    ['Jan-2013', 359333],
+    ['Feb-2013', 321469],
+    ['Mar-2013', 67780],
+    ['Apr-2013', 471435],
+    ['May-2013', 565603],
+    ['Jun-2013', 872480],
+    ['Jul-2013', 789480],
+    ['Aug-2013', 999942],
+    ['Sep-2013', -1196225],
+    ['Oct-2013', 268997],
+    ['Nov-2013', -687986],
+    ['Dec-2013', 1150461],
+    ['Jan-2014', 682458],
+    ['Feb-2014', 617856],
+    ['Mar-2014', 824098],
+    ['Apr-2014', 581943],
+    ['May-2014', 132864],
+    ['Jun-2014', 448062],
+    ['Jul-2014', 689161],
+    ['Aug-2014', 800701],
+    ['Sep-2014', 1166643],
+    ['Oct-2014', 947333],
+    ['Nov-2014', 578668],
+    ['Dec-2014', 988505],
+    ['Jan-2015', 1139715],
+    ['Feb-2015', 1029471],
+    ['Mar-2015', 687533],
+    ['Apr-2015', -524626],
+    ['May-2015', 158620],
+    ['Jun-2015', 87795],
+    ['Jul-2015', 423389],
+    ['Aug-2015', 840723],
+    ['Sep-2015', 568529],
+    ['Oct-2015', 332067],
+    ['Nov-2015', 989499],
+    ['Dec-2015', 778237],
+    ['Jan-2016', 650000],
+    ['Feb-2016', -1100387],
+    ['Mar-2016', -174946],
+    ['Apr-2016', 757143],
+    ['May-2016', 445709],
+    ['Jun-2016', 712961],
+    ['Jul-2016', -1163797],
+    ['Aug-2016', 569899],
+    ['Sep-2016', 768450],
+    ['Oct-2016', 102685],
+    ['Nov-2016', 795914],
+    ['Dec-2016', 60988],
+    ['Jan-2017', 138230],
+    ['Feb-2017', 671099]
 ];
 
-/*
-The total number of months included in the dataset.
-Calculated with the .lenght JavaScript property
-*/
-const totalMonths = finances.length
-
-/*
-The net total amount of Profit/Losses over the entire period.
-Calculated by creating a new array with only the numbers,
-with the reduce method I have added the sum of all the numbers in the onlyNumber array
-into a new array netTotalAmmount
-*/
-const onlyNumbers = []
+let maxProfit = 0
+let minProfit = 0
+let maxProfitMonth
+let minProfitMonth
+let totalChangeInProfits = 0
+let totalProfit = 0
+let totalMonths = finances.length
 
 for (let i = 0; i < finances.length; i++) {
-    onlyNumbers.push(finances[i][1])
-}
-
-const netTotalAmount = onlyNumbers.reduce((a, b) => a + b, 0)
-
-/*
-The greatest increase in profits (date and amount) over the entire period.
-The greatest decrease in losses (date and amount) over the entire period.
-Calculated by getting the highest and lowest number in the onlyNumbers array with
-the Math.max and Math.min methods. Finding the correspondent numbers in the dataset by looping through
-the dataset and pushing it to the maxIncrease and minIncrease variables with a conditional statement
-*/
-const highestNumber = Math.max.apply(null, onlyNumbers)
-const lowestNumber = Math.min.apply(null, onlyNumbers)
-let maxIncrease = []
-let maxDecrease = []
-for (let i = 0; i < finances.length; i++) {
-    if (finances[i][1] === highestNumber) {
-        maxIncrease.push(finances[i])
-        
-    } else if (finances[i][1] === lowestNumber){
-        maxDecrease.push(finances[i])
+    if (i > 0) {
+        if ((finances[i][1] - finances[i - 1][1]) > maxProfit) {
+            maxProfit = (finances[i][1] - finances[i - 1][1])
+            maxProfitMonth = (finances[i][0])
+        }
+        if ((finances[i][1] - finances[i - 1][1]) < minProfit) {
+            minProfit = (finances[i][1] - finances[i - 1][1])
+            minProfitMonth = (finances[i][0])
+        }
+        totalChangeInProfits += (finances[i][1] - finances[i - 1][1])
     }
-    
+    totalProfit += finances[i][1]
 }
 
-// Adds the analisys to the HTML
-const analisys = document.querySelector(".analisys")
-analisys.innerHTML = `
-<p>
-Financial Analisys<br>
----------------------------------<br>
-Total Months: ${totalMonths}<br>
-Total: $${netTotalAmount}<br>
-Greatest Increase in Profits: ${maxIncrease[0][0]} ($${maxIncrease[0][1]})<br>
-Greatest Decrease in Profits: ${maxDecrease[0][0]} ($${maxDecrease[0][1]})
-</p>
-`
+let averageChangeInProfits = totalChangeInProfits / (finances.length - 1)
 
 // Prints the analisys to the console
 console.log(
-`
-Financial Analisys
----------------------------------
+    `
+Financial Analysis
+
+----------------------------
+
 Total Months: ${totalMonths}
-Total: $${netTotalAmount}
-Greatest Increase in Profits: ${maxIncrease[0][0]} ($${maxIncrease[0][1]})
-Greatest Decrease in Profits: ${maxDecrease[0][0]} ($${maxDecrease[0][1]})
+
+Total: ${totalProfit}
+
+Average Change: $${averageChangeInProfits.toFixed(2)}
+
+Greatest Increase in Profits: ${maxProfitMonth} $${maxProfit})
+
+Greatest Decrease in Profits: ${minProfitMonth} $${minProfit}}
+
 `
 )
+
+// Adds the analisys to the HTML
+const analisys = document.querySelector(".analisys")
+analisys.innerHTML =
+    `
+<h1>Console Finances</h1>
+<h2>Financial Analysis</h2>
+
+----------------------------
+<p>Total Months: ${totalMonths}<p>
+<p>Total: ${totalProfit}<p>
+<p>Average Change: $${averageChangeInProfits.toFixed(2)}<p>
+<p>Greatest Increase in Profits: ${maxProfitMonth} $${maxProfit})<p>
+<p>Greatest Decrease in Profits: ${minProfitMonth} $${minProfit}}<p>
+`
